b8ca1844c47dddc46447e79c3bddcda5da19bd3e
Matt Maguire <mhmaguire0@gmail.com>
2015-10-21 16:44:47 -0700

    comment out add wish to cart override

--- app/overrides/add_wish_to_cart_form.rb
+++ app/overrides/add_wish_to_cart_form.rb
@@ -1,7 +1,7 @@
-Deface::Override.new(
-  virtual_path: 'spree/products/show',
-  name: 'add_wish_to_cart_form',
-  insert_bottom: "[data-hook='cart_form']",
-  partial: 'spree/products/wishlist_form',
-  disable: true
-)
+# Deface::Override.new(
+#   virtual_path: 'spree/products/show',
+#   name: 'add_wish_to_cart_form',
+#   insert_bottom: "[data-hook='cart_form']",
+#   partial: 'spree/products/wishlist_form',
+#   disable: true
+# )
